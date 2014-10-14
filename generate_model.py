5aaad7f78a8b0931ba10243372b2ef1e64c11419
Andy Bartholomew <andy.bartholomew@gmail.com>
2014-10-14 11:39:43 -0700

    got something, need to use templates

--- generate_model.py
+++ generate_model.py
@@ -1,44 +1,62 @@
 #!/bin/python
 import json
 
+
 def printClass(class_json):
     props = class_json["properties"]
     print class_json["class_type"] + " " + class_json["classname"] + " {"
-    for property in props:
+    for property in props.items():
         print "\tvar " + property[0] + ": " + property[1]
 
-    init_args = [property[0] + ": " + property[1] for property in props]
+    init_args = [property[0] + ": " + property[1] for property in props.items()]
     print "\tinit("+", ".join(init_args)+") {"
-    for p in props:
-        print "\t\tself."+p[0]+" = "+p[0]
+    for prop_name in props.keys():
+        print "\t\tself."+prop_name+" = "+prop_name
     print "\t}"
     
     print "}"
 
-def printDeserializer(class_json):
+def validateMapping(class_json, json_to_property):
+    mapped_properties = json_to_property.values()
+    for prop_name in class_json["properties"].keys():
+        if prop_name not in mapped_properties:
+            print "// missing "+prop_name
+            return False
+    return True
+
+
+def printDeserializer(class_json, json_key_to_property):
     classname = class_json["classname"]
     props = class_json["properties"]
 
+    # function declaration
     print "func "+classname+"FromJSON(json: NSDictionary)",
     print " -> ("+classname+"?, NSError?) {"
-    # first validate the arguments
-    for prop in props: 
-        print "\tif (json[\""+prop[0]+"\"] is "+prop[1]+") == false {"
-        reason = "[\"reason\":\"+invalid "+prop[0]+"\"]"
+
+    # property type checks
+    for json_key, prop_name in json_key_to_property.items(): 
+        prop_type = props[prop_name]
+        print "\tif (json[\""+json_key+"\"] is "+prop_type+") == false {"
+        reason = "[\"reason\":\"invalid "+json_key+"\"]"
         print "\t\tlet e = NSError(domain:\"com.modeljson.error\", code:-1, userInfo:"+reason+")"
         print "\t\treturn (nil, e)"
         print "\t}"
+
     # then instantiate it with forced casts
-    args_with_casts = ["json[\""+prop[0]+"\"]" + " as " + prop[1] for prop in props]
+    args_with_casts = [prop_name+": json[\""+json_key+"\"]" + " as " + props[prop_name] for (json_key, prop_name) in json_key_to_property.items()]
     print "\treturn ("+classname+"("+", ".join(args_with_casts)+"), nil)"
     print "}"
-    
-    
+
 raw_json_string = open('./example/ExampleSpec.json', 'r').read()
 example_json = json.loads(raw_json_string)
+raw_mapping_string = open('./example/ExampleMapping.json', 'r').read()
+json_key_to_property = json.loads(raw_mapping_string)
+
+if not validateMapping(example_json, json_key_to_property):
+    print "// Missing required properties in mapping"
 
 print "import Foundation"
 print
 printClass(example_json)
 print
-printDeserializer(example_json)
+printDeserializer(example_json, json_key_to_property)
